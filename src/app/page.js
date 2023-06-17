16ef94767c4e175ab43a1413fde9815dd9729e9e
Matt <siglerm37@gmail.com>
2023-06-17 11:57:39 -0700

    Added buttons and routing to Home. Created routing from home to specified pages on button click. Added login and signup page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,26 @@
-import Link from 'next/link'
+import Link from "next/link";
+import styles from "./styles/Home.module.css";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="flex justify-center m-5 text-4xl font-bold">Choose Your Own Adventure AI</h1>
-      <Link href="/game" className="flex justify-center">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Start Game</button>
-      </Link>
+    <main className={styles.homePage}>
+      <div className={styles.container}>
+        <h1>Adventure AI</h1>
+        <div className={styles.textContainer}>
+          <p>Choose your own adventure</p>
+          <div className={styles.buttonContainer}>
+            <Link href="/game">
+              <button className={styles.standardButton}>Play Now!</button>
+            </Link>
+            <Link href="/login">
+              <button className={styles.standardButton}>Log in</button>
+            </Link>
+            <Link href="/signup">
+              <button className={styles.signupButton}>Sign up</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
