0a99c0bcddd872aadcf7643fa3a32fbf11d3d2a4
Matt <siglerm37@gmail.com>
2023-06-17 14:10:22 -0700

    Styled Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -3,22 +3,24 @@ import styles from "./styles/Home.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.homePage}>
-      <div className={styles.container}>
-        <h1>Adventure AI</h1>
-        <div className={styles.textContainer}>
-          <p>Choose your own adventure</p>
-          <div className={styles.buttonContainer}>
-            <Link href="/game">
-              <button className={styles.standardButton}>Play Now!</button>
-            </Link>
-            <Link href="/login">
-              <button className={styles.standardButton}>Log in</button>
-            </Link>
-            <Link href="/signup">
-              <button className={styles.signupButton}>Sign up</button>
-            </Link>
-          </div>
+    <main className="flex justify-center items-center h-screen w-screen">
+      <div className="bg-gray-200 p-10 rounded-lg">
+        <h1 className="text-4xl font-bold text-center">Adventure AI</h1>
+        <p className="text-l text-center">Choose your own adventure</p>
+        <div className="flex flex-col items-center">
+          <Link href="/game">
+            <button className="bg-deep-forest-green hover:bg-moss-green text-white font-bold py-2 px-4 rounded my-1">
+              Play Now!
+            </button>
+          </Link>
+          <Link href="/login">
+            <button className="bg-deep-forest-green hover:bg-moss-green text-white font-bold py-2 px-4 rounded my-1">
+              Log in
+            </button>
+          </Link>
+          <Link href="/signup">
+            <button className="text-center my-1">Sign up</button>
+          </Link>
         </div>
       </div>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "deep-forest-green": "#1B4332",
+        "moss-green": "#4C6A47",
       },
     },
   },
   plugins: [],
-}
+};
